99e2dad5d621c0d5699761f87f55e1ccf90ee687
ElasticEmail <integrations@elasticemail.com>
2026-08-27 04:03:40 +0000

    Release 4.2.0 - realign all package versions

--- src/ElasticEmail/Client/Configuration.cs
+++ src/ElasticEmail/Client/Configuration.cs
@@ -34,7 +34,7 @@ namespace ElasticEmail.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "4.0.30";
+        public const string Version = "4.2.0";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -113,7 +113,7 @@ namespace ElasticEmail.Client
         public Configuration()
         {
             Proxy = null;
-            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/4.0.30/csharp");
+            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/4.2.0/csharp");
             BasePath = "https://api.elasticemail.com/v4";
             DefaultHeaders = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -536,7 +536,7 @@ namespace ElasticEmail.Client
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
             report += "    Version of the API: 4.0.0\n";
-            report += "    SDK Package Version: 4.0.30\n";
+            report += "    SDK Package Version: 4.2.0\n";
 
             return report;
         }
